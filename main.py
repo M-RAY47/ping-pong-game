f196169318a4a9e3f4b8eede0779d5b742b529bf
M.RAY <a.mray47@yahoo.fr>
2021-12-07 23:10:38 +0800

    add the move paddle function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ball import Ball
 
 time.sleep(0.1)
 screen = Screen()
-screen.setup(width=800,height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
 rd_pawn = Pawn()
--- pawn.py
+++ pawn.py
@@ -7,3 +7,6 @@ class Pawn(Turtle):
 		self.color("white")
 		self.penup()
 		self.shapesize(stretch_wid=5,stretch_len=1)
+	def go_up(self):
+		new_y = self.y_cor() + 20
+		self.goto(self.x_cor() + new_y)
