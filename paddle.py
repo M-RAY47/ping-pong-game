03a560833e6d04ca4cd608b04c4c755035a72b6b
M.RAY <a.mray47@yahoo.fr>
2021-12-07 23:22:36 +0800

    Modified paddle class

--- paddle.py
+++ paddle.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-	def __init__(self):
+	def __init__(self, position):
 		super().__init__()
 		self.shape("square")
 		self.color("white")
 		self.penup()
 		self.shapesize(stretch_wid=5,stretch_len=1)
+		self.goto(position)
+		
 	def go_up(self):
 		new_y = self.y_cor() + 20
 		self.goto(self.x_cor(), new_y)
